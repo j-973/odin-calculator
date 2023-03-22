a5aee620058752ec960ac0ecb0e3cde179dc358f
j-973 <jvoorheis973@gmail.com>
2023-03-22 09:49:07 -0400

    created firstNumber, operator, and secondNumber variables. created operate function that calls the function corresponding to any of the 4 operators the user inputs alongside their firstNumber and secondNumber inputs.

--- script.js
+++ script.js
@@ -1,18 +1,43 @@
-const add = (a, b) => {
+//Matching the "3 + 5" example given on the instructions page
+let firstNumber = 3;
+let operator = "+";
+let secondNumber = 5;
+
+add = (a, b) => {
     return a + b;
-  };
+};
   
-  const subtract = (a, b) => {
+subtract = (a, b) => {
     return a - b;
-  };
+};
   
-  const multiply = (a, b) => {
+multiply = (a, b) => {
     return a * b;
-  };
+};
   
-  const divide = (numerator, denominator) => {
+divide = (numerator, denominator) => {
     if (denominator === 0) {
       return 'Error: Cannot divide by zero';
     }
     return numerator / denominator;
-  };
+};
+
+operate = (firstNumber, operator, secondNumber) => {
+  switch (operator) {
+    case "+":
+      return add(firstNumber, secondNumber);
+    case "-":
+      return subtract(firstNumber, secondNumber);
+    case "*":
+      return multiply(firstNumber, secondNumber);
+    case "/":
+      return divide(firstNumber, secondNumber);
+  }
+}
+
+//TESTING operate function:
+console.log(operate(3, "+", 5)); //8
+console.log(operate(3, "-", 5)); //-2
+console.log(operate(3, "*", 5)); //15
+console.log(operate(10, "/", 5)); //2
+console.log(operate(3, "/", 0)); //Error: cannot divide by zero
